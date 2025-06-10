508568b8bcf3f76e73e794d610de0caceb588fdd
saarahk0 <saarahkhan583@gmail.com>
2025-06-10 10:08:33 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,1 +1,12 @@
+from sqlalchemy import Column, Integer, String, Float, Boolean
+from database import Base
+
+class Book(Base):
+    __tablename__ = "books"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    author = Column(String)
+    price = Column(Float)
+    in_stock = Column(Boolean)
 
